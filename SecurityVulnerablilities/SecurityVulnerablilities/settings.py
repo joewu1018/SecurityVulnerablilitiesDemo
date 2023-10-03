d15fe0dd212f843e27408f94c983cf9c03cbdeff
THChen <terryhorse2002@gmail.com>
2023-10-03 21:01:54 +0800

    featue-mssqlSettings 1.設定mssql 2.新增requirements 3.新增.env

--- SecurityVulnerablilities/SecurityVulnerablilities/settings.py
+++ SecurityVulnerablilities/SecurityVulnerablilities/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1hzg)_!389wvmnah2dog^v%5uo%y0(j@mxsgz_h4g^40fu%iiz'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,9 +78,20 @@ WSGI_APPLICATION = 'SecurityVulnerablilities.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'SVDemo',
+        'USER': os.getenv("MSSQL_USER"),
+        'PASSWORD': os.getenv("MSSQL_PASSWORD"),
+        'HOST': '127.0.0.1',
+        'PORT': '1433',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        },
     }
 }
 
