8ea23c430bae5173c06668a2986f7ef9f92bbc48
THChen <terryhorse2002@gmail.com>
2023-10-09 23:17:11 +0800

    feature-StudentCRUD 1.新增Student Model 2.新增、修改頁面init

--- SecurityVulnerablilities/accounts/views.py
+++ SecurityVulnerablilities/accounts/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(requests):
-    return render(requests, "accounts/index.html")
+def index(request):
+    return render(request, "accounts/index.html")
 
 def xss_vulnerable(request):
     if request.method == 'POST':
         message = request.POST.get('message')
     return render(request, 'accounts/xss.html', locals())
+
+def search(request):
+    return render(request, "accounts/search.html")
+
+def update(request):
+    return render(request, "accounts/update.html")
