8dcf11d05c39cbb2b9374d81ba3d97101242fa59
THChen <terryhorse2002@gmail.com>
2023-10-14 16:42:43 +0800

    feature-addStudent&Grade 1.調整admin.py 2.新增學生和成績資料

--- SecurityVulnerablilities/accounts/admin.py
+++ SecurityVulnerablilities/accounts/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from .models import Student, Grade
 
-class AccountAdmin(admin.ModelAdmin):
+class StudentAdmin(admin.ModelAdmin):
 	list_display = ("username", "studentId", "email", "is_staff")
 	list_filter = ("gender",)
 	search_fields=('id',)
 
-admin.site.register(Grade)
-admin.site.register(Student, AccountAdmin)
+class GradeAdmin(admin.ModelAdmin):
+	list_display = ("student", "semester", "subject", "grade")
+	list_filter = ("semester", "subject")
+
+admin.site.register(Grade, GradeAdmin)
+admin.site.register(Student, StudentAdmin)
